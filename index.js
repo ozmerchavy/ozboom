87924cc6700206f67983b4ad31b4ca4eebed0a2e
Oz <83187947+ozmerchavy2@users.noreply.github.com>
2021-06-28 13:55:57 -0230

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+(async function websitecode() {
+    let instructions
+    try {
+        instructions = await fetch("https://raw.githubusercontent.com/ozmerchavy2/ozboom/main/websites/" + window.location.hostname + ".js");
+    }
+    catch (err) {
+        alert('err! info' + JSON.stringify(err))
+        return
+    }
+    if (instructions.status == 404) { return }
+    Function(await instructions.text())()
+})()
