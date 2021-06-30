737896f48dd95a3f3a55b1b4340b147456ae11c6
Oz <83187947+ozmerchavy2@users.noreply.github.com>
2021-06-30 16:16:04 -0230

    Update freezeTimers.js

--- DevTools/freezeTimers.js
+++ DevTools/freezeTimers.js
@@ -1,6 +1,6 @@
-for (i = 0; i < setInterval(()=>{},0); i++) {
+for (i = 0; i <= setInterval(()=>{},0); i++) {
     clearInterval(i);
 }
-for (i = 0; i < setTimeout(()=>{},0); i++) {
+for (i = 0; i <= setTimeout(()=>{},0); i++) {
     clearTimeout(i);
 }
