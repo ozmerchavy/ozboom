2b030ddf8f4d887fb19d891379a661344c1c2a28
Oz <83187947+ozmerchavy2@users.noreply.github.com>
2021-06-30 15:48:48 -0230

    Update addSuperFetchFunction.js

--- DevTools/addSuperFetchFunction.js
+++ DevTools/addSuperFetchFunction.js
@@ -1,5 +1,12 @@
-async function superfetch%28t%2Ce%29%7Blet r%3D%60https//notes-get-req.herokuapp.com/fetch?url=${t}`;e&&(r+=`&options=${JSON.stringify(e)}`);const n=await fetch(r),s=await n.text()
-  ,a=JSON.parse(s).text;return(new DOMParser).parseFromString(a,"text/html")}
+async function superFetch(url, options = undefined) {
+    let superUrl = `https://notes-get-req.herokuapp.com/fetch?url=${url}`;
+    if (options) {
+        superUrl += `&options=${JSON.stringify(options)}`;
+    }
+    const theFetch = await fetch(superUrl);
+    const theText = await theFetch.text();
+    return JSON.parse(theText);
+}
 async function trySuperFetch(url,options=undefined){let superUrl=`https://notes-get-req.herokuapp.com/fetch?url=${url}`;if(options){superUrl+=`&options=${JSON.stringify(options)}`}console.log("tried")}
 async function checkLink(t){const e=await fetch(t),n=await e.text();return(new DOMParser).parseFromString(n,"text/html")}
 
