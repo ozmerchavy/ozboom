ceb6d692313e85dfe54e478e9175c19dbfef37e0
Oz <83187947+ozmerchavy2@users.noreply.github.com>
2021-06-29 22:37:47 -0230

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,42 @@
 async function websitecode() {
-    let instructions
-    try {
-        instructions = await fetch("https://raw.githubusercontent.com/ozmerchavy2/ozboom/main/websites/" + window.location.hostname + ".js");
-    }
-    catch (err) {
-        alert('err! info' + JSON.stringify(err))
-        return
-    }
-    if (instructions.status == 404) { alert("error:404"); return }
-    Function(await instructions.text())()
+      let instructions
+      try {
+            instructions = await fetch("https://raw.githubusercontent.com/ozmerchavy2/ozboom/main/websites/" + window.location.hostname + ".js");
+      }
+      catch (err) {
+            alert('err! info' + JSON.stringify(err))
+            return
+      }
+      if (instructions.status == 404) { alert("error:404"); return }
+      Function(await instructions.text())()
 } /////////////////////////////////////////Teaches how to do the OzBoom
 
 if (!window.clickLastSec) {
-    clickLastSec = 0
+      clickLastSec = 0
 } ///////////////////////////////////////Adds clickLastSec as a thingy thing 
 clickLastSec++//////////////////////////indicates a click
 
 async function stuffToDo() {
-    if (clickLastSec < 3) {
-        await websitecode()
-    }
-    if (clickLastSec == 3) {
-        console.log("Easter egg")
-        if(editMode==true){
-            document.body.contentEditable = 'false'; document.designMode = 'off'; void 0  
-            return 
-        }
-        editMode =true
-        document.body.contentEditable = 'true'; document.designMode = 'on'; void 0
-    }
-    if (clickLastSec == 4) {
-        console.log("Easter egg boom special")
-    }
-   
-    clickLastSec = 0
+      if (clickLastSec < 3) {
+            await websitecode()
+      }
+      if (clickLastSec == 3) {
+            console.log("Easter egg")
+            if (!window.editMode) {
+                  editMode = 0
+            }
+            if (editMode == true) {
+                  document.body.contentEditable = 'false'; document.designMode = 'off'; void 0
+                  return
+            }
+            editMode = true
+            document.body.contentEditable = 'true'; document.designMode = 'on'; void 0
+      }
+      if (clickLastSec == 4) {
+            console.log("Easter egg boom special")
+      }
+
+      clickLastSec = 0
 }
 
 setTimeout(() => { if (clickLastSec > 0) { stuffToDo() } }, 1000)
