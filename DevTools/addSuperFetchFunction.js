1b34ca5c53f360a548b6d0ced7e108c049c8cc45
Oz <83187947+ozmerchavy2@users.noreply.github.com>
2021-07-01 10:09:19 -0230

    Update addSuperFetchFunction.js

--- DevTools/addSuperFetchFunction.js
+++ DevTools/addSuperFetchFunction.js
@@ -10,5 +10,8 @@ superFetch = async function superFetch(url, options = undefined) {
 trySuperFetch= async function trySuperFetch(url,options=undefined){let superUrl=`https://notes-get-req.herokuapp.com/fetch?url=${url}`;if(options){superUrl+=`&options=${JSON.stringify(options)}`}console.log("tried")}
 checkLink = async function checkLink(t){const e=await superFetch(t),n=await e.text();return(new DOMParser).parseFromString(n,"text/html")}
 
+randomArticleName = async function randomArticleName(){
+let wiki = await superFetch("https://en.wikipedia.org/wiki/Special:Random")
+return wiki.text.split("title>")[1].split(" - Wikipedia</")[0]}
 
-console.log("added async functions: trySuperFetch, superFetch and checkLink (that returns a document)")
+console.log("added async functions: trySuperFetch, superFetch, checkLink (that returns a document - rn doesnt work), and RandomArticleName() too")
