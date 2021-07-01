908c4ea70b06d066ca9645750c3708326928b7ed
Oz <83187947+ozmerchavy2@users.noreply.github.com>
2021-07-01 10:01:27 -0230

    Update addSuperFetchFunction.js

--- DevTools/addSuperFetchFunction.js
+++ DevTools/addSuperFetchFunction.js
@@ -8,7 +8,7 @@ superFetch = async function superFetch(url, options = undefined) {
     return JSON.parse(theText);
 }
 trySuperFetch= async function trySuperFetch(url,options=undefined){let superUrl=`https://notes-get-req.herokuapp.com/fetch?url=${url}`;if(options){superUrl+=`&options=${JSON.stringify(options)}`}console.log("tried")}
-checkLink = async function checkLink(t){const e=await fetch(t),n=await e.text();return(new DOMParser).parseFromString(n,"text/html")}
+checkLink = async function checkLink(t){const e=await superFetch(t),n=await e.text();return(new DOMParser).parseFromString(n,"text/html")}
 
 
 console.log("added async functions: trySuperFetch, superFetch and checkLink (that returns a document)")
