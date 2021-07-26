4828a634b84e42d2d36b01967af89d9cf54d1c2a
Oz <83187947+ozmerchavy2@users.noreply.github.com>
2021-07-25 23:41:28 -0230

    Update addUsefulFunctions.js

--- DevTools/addUsefulFunctions.js
+++ DevTools/addUsefulFunctions.js
@@ -1,63 +1,153 @@
+let allsuspects = [...document.querySelectorAll("h2", "h3", "h4", "h5", "a", "th", "td")]
+if (allsuspects<1){alert("couldn't find names of shows:( I am sorry, I am really not perfect. try me on another website please!")}
+let originalT = document.title
+let kaki = new Date
 say = console.log
-sleep = (ms)=> new Promise(res => (setTimeout(res,ms)))
-superFetch = async function superFetch(url, options = undefined) {
-    let superUrl = `https://notes-get-req.herokuapp.com/fetch?url=${url}`;
-    if (options) {
-        superUrl += `&options=${JSON.stringify(options)}`;
-    }
-    const theFetch = await fetch(superUrl);
-    const theText = await theFetch.text();
-    return JSON.parse(theText);
-}
-trySuperFetch= async function trySuperFetch(url,options=undefined){let superUrl=`https://notes-get-req.herokuapp.com/fetch?url=${url}`;if(options){superUrl+=`&options=${JSON.stringify(options)}`}console.log("tried")}
-checkLink = async function checkLink(t){const e=await superFetch(t),n=await e.text();return(new DOMParser).parseFromString(n,"text/html")}
+sleep = (ms) => new Promise(res => (setTimeout(res, ms)))
 
-randomArticleName = async function randomArticleName(){
-let wiki = await superFetch("https://en.wikipedia.org/wiki/Special:Random")
-return wiki.text.split("title>")[1].split(" - Wikipedia</")[0]}
 
-function downloadJSON(filename, string) {
-    // thanks Matěj Pokorný from stack overflow
-    
-    const a = document.createElement('a');
-    const href = 'data:application/json;charset=utf-8,' + encodeURIComponent(string);
-    a.setAttribute('href', href);
-    a.setAttribute('download', filename);
-    a.style.display = 'none';
-
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-}
-
-function downloadText(filename, string) {
-    // thanks Matěj Pokorný from stack overflow
-
-    const a = document.createElement('a');
-    const href = 'text/plain;charset=utf-8,' + encodeURIComponent(string);
-    a.setAttribute('href', href);
-    a.setAttribute('download', filename);
-    a.style.display = 'none';
-
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-}
-
-amazingFetch = async function(url, options = undefined) {
+amazingFetch = async function (url, options = undefined) {
     let superUrl = `https://notes-get-req.herokuapp.com/fetch?url=${url}`;
     if (options) {
         superUrl += `&options=${JSON.stringify(options)}`;
     }
     const theFetch = await fetch(superUrl);
     const { status, text } = JSON.parse(await theFetch.text());
-    return { 
+    return {
         status,
         text: () => Promise.resolve(text)
     };
 }
 
+async function getShowlink(keyword) {
+    let yap = encodeURI(keyword)
 
-console.log("added async functions: trySuperFetch, superFetch, checkLink (that returns a document - rn doesnt work), and RandomArticleName() and downloadTEXT downloadJSON too and also amazingFetch() that does return an object too with the function text()")
+    let res = await amazingFetch(`https://www.sdarot.tv/search?term=${yap}`)
+    let text = await res.text()
+    if (text.includes('לא נמצאו')) { return null }
+    let docu = new DOMParser().parseFromString(text, "text/html")
+    let b = docu.querySelector(`a[href*=watch]`)
+    if (!b) { // if no link found (error page)
+        console.log('pipi bamichnas', keyword)
+        return null
+    }
+    else {
+        let link = 'https://sdarot.tv/watch' + b.toString().split("/watch")[1]
+        return link
+    }
+}
+
+async function googleShowLink(keyword) {
+    let yap = encodeURI(keyword+"+sdarot.tv/")
+    let results = []
+    let res = await amazingFetch(`https://www.google.com/search?as_q=${yap}`)
+    let text = await res.text()
+    let docu = new DOMParser().parseFromString(text, "text/html")
+    docu.querySelectorAll("a").forEach(a=>{ if ( a.outerHTML.includes("https://www.sdarot.tv/watch/")){results.push(a.href)}})
+    if (results.length< 1)  {
+        console.log('google pipi bamichnas', keyword)
+        return null
+    }
+    else {
+        return 'https://sdarot.tv/watch' + results[0].toString().split("/watch")[1] 
+    }
+}
+
+async function getEnglishlink(keyword) {
+    let yap = encodeURI(keyword)
+
+    let res = await amazingFetch(`https://primewire.pics/search/${yap}`)
+    let text = await res.text()
+    if (text.includes('no results')) { return null }
+    let link = `https://primewire.pics/search/${yap}`
+    return link
+
+}
+
+async function findHebrew() {
+    while (allsuspects.length > 0) {
+        const h = allsuspects.shift()
+        let name = h.innerText.match(/[a-zA-Z ]+/g)?.[0];
+        if (name) {
+            document.title = "scaning:"+name
+
+            let link = await getShowlink(name)
+            if (link) {
+                say('got the link', link)
+                h.querySelectorAll("*").forEach(thing => thing.href = "#")
+                h.style.border = "3px solid yellow"
+                h.style.cursor = "pointer"
+                h.style.textDecoration = 'underline'
+                h.onclick = function () { open(link) }
+            }
+            else {
+                allsuspects.push(h)
+            }
+            await sleep(Math.floor(Math.random() * 250 + 400))
+            if (new Date - kaki > 1000 * 60 * 2) { say("I need to do pipi");document.title = originalT; break }
+        }
+
+    }
+}
+async function googleHebrew() {
+    while (allsuspects.length > 0) {
+        const h = allsuspects.shift()
+        let name = h.innerText.match(/[a-zA-Z ]+/g)?.[0];
+
+        if (name) {
+            document.title = "scaning:"+name
+
+            let link = await googleShowLink(name)
+            if (link) {
+                say('google got the link!', link)
+                h.querySelectorAll("*").forEach(thing => thing.href = "#")
+                h.style.border = "3px solid yellow"
+                h.style.cursor = "pointer"
+                h.style.textDecoration = 'underline'
+                h.onclick = function () { open(link) }
+            }
+            else {
+                allsuspects.push(h)
+            }
+            await sleep(Math.floor(Math.random() * 50 + 10))
+            if (new Date - kaki > 1000 * 60 * 2) { say("I need to do kaki");document.title = originalT; break }
+        }
+
+    }
+}
+
+async function findEnglish() {
+        while (allsuspects.length > 0) {
+            const h = allsuspects.shift()
+            if (h.style.border == "3px solid yellow") { allsuspects.push(h) }
+            else{
+            let name = h.innerText.match(/[a-zA-Z ]+/g)?.[0];
+            if (name) {
+                document.title = "scaning:"+name
+
+                let link = await getEnglishlink(name)
+                if (link) {
+                    say('got the English link', link)
+                    h.querySelectorAll("*").forEach(thing => thing.href = "#")
+                    h.style.border = "3px solid blue"
+                    h.style.cursor = "pointer"
+                    h.style.textDecoration = 'underline'
+                    h.onclick = function () { open(link) }
+                    allsuspects.push(h)
+
+                }
+                else {
+                    allsuspects.push(h)
+                }
+                await sleep(Math.floor(Math.random() * 50 + 50))
+                if (new Date - kaki > 1000 * 60 * 2) { say("I need to do pipi"); document.title = originalT; break }
+            }
+
+        }}
+    }
 
 
+console.log("scanning:", allsuspects)
+findHebrew()
+googleHebrew()
+findEnglish()
