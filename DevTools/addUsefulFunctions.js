09748a3cadb9d3e11833f0a36c538f7cf2af0fab
Oz <83187947+ozmerchavy2@users.noreply.github.com>
2021-07-26 09:46:02 -0230

    Update addUsefulFunctions.js

--- DevTools/addUsefulFunctions.js
+++ DevTools/addUsefulFunctions.js
@@ -9,8 +9,7 @@ superFetch = async function superFetch(url, options = undefined) {
     const theText = await theFetch.text();
     return JSON.parse(theText);
 }
-trySuperFetch= async function trySuperFetch(url,options=undefined){let superUrl=`https://notes-get-req.herokuapp.com/fetch?url=${url}`;if(options){superUrl+=`&options=${JSON.stringify(options)}`}console.log("tried")}
-checkLink = async function checkLink(t){const e=await superFetch(t),n=await e.text();return(new DOMParser).parseFromString(n,"text/html")}
+checkLink = async function checkLink(t){const e=await amazingFetch(t),n=await e.text();return(new DOMParser).parseFromString(n,"text/html")}
 
 randomArticleName = async function randomArticleName(){
 let wiki = await superFetch("https://en.wikipedia.org/wiki/Special:Random")
@@ -59,4 +58,4 @@ amazingFetch = async function(url, options = undefined) {
 
 
 
-console.log("added async functions: trySuperFetch, superFetch, checkLink (that returns a document - rn doesnt work), and RandomArticleName() and downloadTEXT downloadJSON too and also amazingFetch() that does return an object too with the function text()")
+console.log("added async functions: amazingFetch, superFetch (its old version), checkLink (that returns a document - rn doesnt work), and RandomArticleName() and downloadTEXT downloadJSON)
