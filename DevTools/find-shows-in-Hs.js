9b957ca1a8524a4ac03c38d0cab2880566d5b4f7
Oz <83187947+ozmerchavy2@users.noreply.github.com>
2021-07-25 23:20:10 -0230

    Update find-shows-in-Hs.js

--- DevTools/find-shows-in-Hs.js
+++ DevTools/find-shows-in-Hs.js
@@ -1,4 +1,5 @@
-let allsuspects = [...document.querySelectorAll("h2", "h3", "h4", "h5", "a", "td", "th" )]
+let allsuspects = [...document.querySelectorAll("h2", "h3", "h4", "h5", "a", "th", "td")]
+
 let kaki = new Date
 say = console.log
 sleep = (ms) => new Promise(res => (setTimeout(res, ms)))
@@ -30,6 +31,22 @@ async function getShowlink(keyword) {
     else {
         let link = 'https://sdarot.tv/watch' + b.toString().split("/watch")[1]
         return link
+    }
+}
+
+async function googleShowLink(keyword) {
+    let yap = encodeURI(keyword+"+sdarot.tv/")
+    let results = []
+    let res = await amazingFetch(`https://www.google.com/search?as_q=${yap}`)
+    let text = await res.text()
+    let docu = new DOMParser().parseFromString(text, "text/html")
+    docu.querySelectorAll("a").forEach(a=>{ if ( a.outerHTML.includes("https://www.sdarot.tv/watch/")){results.push(a.href)}})
+    if (results.length< 1)  {
+        console.log('google pipi bamichnas', keyword)
+        return null
+    }
+    else {
+        return 'https://sdarot.tv/watch' + results[0].toString().split("/watch")[1] 
     }
 }
 
@@ -68,6 +85,30 @@ async function findHebrew() {
 
     }
 }
+async function googleHebrew() {
+    while (allsuspects.length > 0) {
+        const h = allsuspects.shift()
+        let name = h.innerText.match(/[a-zA-Z ]+/g)?.[0];
+        if (name) {
+
+            let link = await googleShowLink(name)
+            if (link) {
+                say('google got the link!', link)
+                h.querySelectorAll("*").forEach(thing => thing.href = "#")
+                h.style.border = "3px solid yellow"
+                h.style.cursor = "pointer"
+                h.style.textDecoration = 'underline'
+                h.onclick = function () { open(link) }
+            }
+            else {
+                allsuspects.push(h)
+            }
+            await sleep(Math.floor(Math.random() * 50 + 10))
+            if (new Date - kaki > 1000 * 60 * 2) { say("I need to do kaki"); break }
+        }
+
+    }
+}
 
 async function findEnglish() {
         while (allsuspects.length > 0) {
@@ -91,12 +132,15 @@ async function findEnglish() {
                 else {
                     allsuspects.push(h)
                 }
-                await sleep(Math.floor(Math.random() * 100 + 100))
+                await sleep(Math.floor(Math.random() * 50 + 50))
                 if (new Date - kaki > 1000 * 60 * 2) { say("I need to do pipi"); break }
             }
 
         }}
     }
 
+
+console.log("scanning:", allsuspects)
 findHebrew()
+googleHebrew()
 findEnglish()
