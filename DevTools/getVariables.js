f86de55eed0c64dc44a711c07947416718bf97fc
Oz <83187947+ozmerchavy2@users.noreply.github.com>
2021-10-24 22:15:07 -0230

    Update getVariables.js

--- DevTools/getVariables.js
+++ DevTools/getVariables.js
@@ -1,4 +1,4 @@
 Object.keys(window).filter(function(x)
 {
-    return window[x] instanceof Function && !/\[native code\]/.test(window[x].toString());
+    return !/\[native code\]/.test(window[x].toString());
 });
