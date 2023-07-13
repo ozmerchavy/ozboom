c5abba6145a9616e54d5fe6fa24c78ea85d57541
Oz <83187947+ozmerchavy2@users.noreply.github.com>
2023-07-12 23:00:44 -0230

    Create ciffc.net.js

---
+++ websites/ciffc.net.js
@@ -0,0 +1,18 @@
+function makeTableKM2ifNecessary(table) {
+  let headers = [...table.querySelectorAll("th")];
+
+  for (let i = 0; i < headers.length; i++) {
+    if (headers[i].innerText === "Hectares") {
+      headers[i].innerHTML = 'km<sup>2</sup>';
+
+      let tds = [...table.querySelectorAll("tr")].map(tr => tr.querySelectorAll("td")[i]);
+
+      tds.forEach(td => {
+        if (td) {
+          td.innerText = Math.round(Number(td.innerText) * 100) / 10000 + " ";
+        }
+      });
+    }
+  }
+}
+[...document.querySelectorAll("table")].forEach(t=>makeTableKM2ifNecessary(t))
