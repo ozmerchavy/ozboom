913b1e6f87036e0518de2d9a0d73b5afa2800534
Oz <83187947+ozmerchavy2@users.noreply.github.com>
2023-07-13 22:53:42 -0230

    Update ciffc.net.js

--- websites/ciffc.net.js
+++ websites/ciffc.net.js
@@ -1,20 +1,34 @@
 console.log("converting from hectars to km2...")
 
+function formatize(innerT){
+   const t =  Math.round(Number(innerT)/ 100) 
+   return t.toLocaleString() + " "
+}
+
 function makeTableKM2ifNecessary(table) {
-  let headers = [...table.querySelectorAll("th")];
+    let headers = [...table.querySelectorAll("th")];
+  
+    for (let i = 0; i < headers.length; i++) {
+      if (headers[i].innerText === "Hectares") {
+        headers[i].innerHTML = 'km<sup>2</sup>';
+  
+        let tds = [...table.querySelectorAll("tr")].map(tr => tr.querySelectorAll("td")[i]);
+  
+        tds.forEach(td => {
+          if (td) {
+            td.innerText = formatize(td.innerText);
+          }
+        });
+  
+        let sum = table.querySelectorAll("tfoot th")[i]
+        console.log(sum)
+        if(sum){
+            sum.innerText = formatize(sum.innerText)
 
-  for (let i = 0; i < headers.length; i++) {
-    if (headers[i].innerText === "Hectares") {
-      headers[i].innerHTML = 'km<sup>2</sup>';
-
-      let tds = [...table.querySelectorAll("tr")].map(tr => tr.querySelectorAll("td")[i]);
-
-      tds.forEach(td => {
-        if (td) {
-          td.innerText = Math.round(Number(td.innerText) * 100) / 10000 + " ";
         }
-      });
+        
+      }
     }
   }
-}
+  
 [...document.querySelectorAll("table")].forEach(t=>makeTableKM2ifNecessary(t))
