66f8888c8fce4039be77168ab511121e0d9bfae9
Oz <83187947+ozmerchavy@users.noreply.github.com>
2025-09-10 16:20:19 -0230

    Update nytimes.com.js

--- websites/nytimes.com.js
+++ websites/nytimes.com.js
@@ -1,1 +1,11 @@
+document.querySelector("#portal-game-modals").remove()
+
+
+const obscuredElements = document.querySelectorAll("ol[class*='obscured'], div[class*='obscured']");
+
+obscuredElements.forEach(element => {
+  // Replace 'obscured' wherever it appears in the class list
+  element.className = element.className.replace(/\bobscured\b/g, "").trim();
+});
+
 document.querySelector("#app div div").className = "FuckNYT", document.getElementById("gateway-content").remove(), document.getElementById("site-content").style = "FuckNYT", document.getElementById("top-wrapper").remove(), document.querySelector(".css-1bd8bfl").remove(), alert("NYC HACKED");
